15cf8d6aa167c78844b226f7b87eb3578262f0ef
Jan Jáchim <jachim5@gmail.com>
2025-06-11 18:08:33 +0200

    fixing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import "./sketch.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``
